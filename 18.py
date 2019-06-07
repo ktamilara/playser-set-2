5742cf56f3a69723de9c69e82a2d81092acfe955
ktamilara <42666475+ktamilara@users.noreply.github.com>
2019-06-07 14:36:07 +0530

    Create 18.py

---
+++ 18.py
@@ -0,0 +1,14 @@
+u1=int(input())
+v1=[]
+x1=0
+z1=0
+for i in range(u1):
+    w1=input()
+    v1.append(w1)
+for i in v1:
+    for y1 in i1:
+        x1+=ord(y1)
+    if x1==612:
+        z1+=1
+    x1=0
+print(z1)
