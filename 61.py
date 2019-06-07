fc22349f46f6281b8bdfaa596c9c3ad096979643
ktamilara <42666475+ktamilara@users.noreply.github.com>
2019-06-07 16:12:36 +0530

    Update 61.py

--- 61.py
+++ 61.py
@@ -1,6 +1,6 @@
 value1=list(map(str,input()))
 value2=list(map(str,input()))
-for i in range(0,len(vauel2)):
+for i in range(0,len(value2)):
     num1=num2=num3=0
     num2=ord(value1[i])
     num3=ord(value2[i])
