31b783b4a1122e330592d7a29016a3eb6b2f2221
ktamilara <42666475+ktamilara@users.noreply.github.com>
2019-06-07 14:14:39 +0530

    Update 15.py

--- 15.py
+++ 15.py
@@ -1,9 +1,5 @@
-k1=input()
-l1=k1.lower()
-m1=0
-n1=0
-for i in range(len(k1)):
-    if l1.count(l1[i])>m1:
-        m1=l.count(l1[i])
-        n1=i
-print(k1[n1])
+s3=input()
+m1=list(s3)
+dict = {j:m1.count(j) for j in m1}
+maxim = max(dict, key=dict.get)  
+print(maxim)
