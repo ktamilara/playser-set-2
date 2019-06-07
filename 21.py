96830f1a7cf2f28d549cf8a90b6116b33c1fbfc9
ktamilara <42666475+ktamilara@users.noreply.github.com>
2019-06-07 15:33:30 +0530

    Create 21.py

---
+++ 21.py
@@ -0,0 +1,11 @@
+n2,k1=map(int,input().split(" "))
+a1,b1=map(int,input().split(" "))
+c1,d1=map(int,input().split(" "))
+if n2==a1==c1:
+	print("yes")
+elif k1==b1==d1:
+	print("yes")
+elif n2==k1 and a1==b1 and c1==1d:
+	print("yes")
+else:
+	print("no")
