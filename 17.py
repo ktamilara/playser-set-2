83e4e49507c1aac462af78dc8045572e88126767
ktamilara <42666475+ktamilara@users.noreply.github.com>
2019-06-07 14:29:42 +0530

    Update 17.py

--- 17.py
+++ 17.py
@@ -1,4 +1,7 @@
-import math
-s,t=input().split()
-e=math.gcd(int(f),int(g))
-print((int(f)*int(g))//e)
+v1,w1=map(int,input().split())
+x3=v*w
+b1=[]
+for i in range(1,x3+1):
+    if i%1v==0 and i%w1==0:
+        b1.append(i)
+print(min(b1))
