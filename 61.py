5f6d216b54cff1c93a7d086f2282d7718c709cde
ktamilara <42666475+ktamilara@users.noreply.github.com>
2019-06-07 16:09:16 +0530

    Create 61.py

---
+++ 61.py
@@ -0,0 +1,18 @@
+value1=list(map(str,input()))
+value2=list(map(str,input()))
+for i in range(0,len(vauel2)):
+    num1=num2=num3=0
+    num2=ord(value1[i])
+    num3=ord(value2[i])
+    num1=num2+num3
+    if num1>96 and num1<123:
+        print(chr(num1),end="")
+    elif (num1-96)<122 and(num1-96)>96:
+        num1=num1-96
+        print(chr(num1),end="")
+    elif num1>148:
+        n1=n1-96-26
+        print(chr(num1), end="")
+    else:
+        num1=num1-26
+        print(chr(num1), end="")
